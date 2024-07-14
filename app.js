7fe492a4c01cded305371a35104dd19717900a17
Gustavo <versace123mx@gmail.com>
2024-07-14 15:29:43 -0600

    primera ruta

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
-import dotenv from 'dotenv';
-import {dbConecction} from './basedatos/conexion.js'
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { dbConecction } from "./basedatos/conexion.js";
 
-dotenv.config();
+dotenv.config(); //iniciando variables de entorno
+dbConecction(); //conexion base de datos
 
-dbConecction()
+//Crear servidor de node
+const app = express();
+const puerto = process.env.PUERTO_EXPRESS || 3000;
+
+//configurar cors
+app.use(cors());
+
+//Convertir body a objeto de js
+app.use(express.json());
+
+//Crear Rutas
+app.get('/probando',(req,res)=>{
+    res.status(200).json({msg:'hola mundo'})
+})
+
+//Crear servidor y escuchar peticiones http
+app.listen(puerto, () => {
+    console.log(`El servidor de espress esta funcionando en el puerto ${puerto}`);
+});
